debb511524e5a6012dffb93b3584f3e1d1ae5685
felipe lussi <felipe-p-l@hotmail.com>
2024-12-26 17:52:53 -0300

    refactor: update db reset task

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import {fileURLToPath} from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const filePath = path.join(__dirname, 'results.csv')
 
 const args = process.argv.slice(2);
@@ -20,47 +19,44 @@ const api = axios.create(
     }
 )
 api.post(`/`).then((res) => {
-    console.log('Ledger created')
-    console.log('Postings        Time')
 })
 
+const transactions = [];
+const destination = `${faker.string.uuid()}:credit:${faker.string.uuid()}`
+for (let j = 1; j <= postingsAmount; j++) {
+    transactions.push({
+        asset: "BRL/2",
+        amount: faker.number.int({max: 1000}),
+        sources: [{account: "world"}],
+        destinations: [{account: destination}],
+    })
+}
+const payload: NumscriptTransaction = {
+    send: transactions,
+    txMeta: {
+        type: 'V2_ACCOUNT_SEND',
+        transactionTypeDescription: 'Envio de pontos',
+        description: "description",
+    },
+    accountMeta: {
+        [destination]: {
+            expirationDate: "2022-11-17T10:20:55Z",
+        },
+    },
+}
 
-    const transactions = [];
-    const destination = `${faker.string.uuid()}:credit:${faker.string.uuid()}`
-    for (let j = 1; j <= postingsAmount; j++) {
-        transactions.push({
-            asset: "BRL/2",
-            amount: faker.number.int({max: 1000}),
-            sources: [{account: "world"}],
-            destinations: [{account: destination}],
-        })
-    }
-    const payload: NumscriptTransaction = {
-        send: transactions,
-        txMeta: {
-            type: 'V2_ACCOUNT_SEND',
-            transactionTypeDescription: 'Envio de pontos',
-            description: "description",
-        },
-        accountMeta: {
-            [destination]: {
-                expirationDate: "2022-11-17T10:20:55Z",
-            },
-        },
-    }
-
-    const parsedPayload = generateNumscript(payload)
-    const transaction = {
-        refreference: `v2:transaction:${faker.string.uuid()}`,
-        timestamp: "2022-11-17T10:20:55Z",
-        script: {
-            plain: parsedPayload,
-        },
-    }
-    const startTime = performance.now();
-    await createTransaction(transaction)
-    const endTime = performance.now();
-    await fs.appendFile(filePath, `${postingsAmount},${endTime - startTime}\n`)
+const parsedPayload = generateNumscript(payload)
+const transaction = {
+    refreference: `v2:transaction:${faker.string.uuid()}`,
+    timestamp: "2022-11-17T10:20:55Z",
+    script: {
+        plain: parsedPayload,
+    },
+}
+const startTime = performance.now();
+await createTransaction(transaction)
+const endTime = performance.now();
+await fs.appendFile(filePath, `${postingsAmount},${endTime - startTime}\n`)
 
 
 async function createTransaction(transaction) {
